c297583fdfe4c14b1e088d9ea06cc18c0215dddf
Nhung Lô <nhungltpk00606@fpt.edu.vn>
2016-10-22 21:59:45 +0700

    chưa hoàn thiện
    chưa hoàn thiện

--- src/com/example/nhungltpk00606/fragment/GiaiTri_fm.java
+++ src/com/example/nhungltpk00606/fragment/GiaiTri_fm.java
@@ -9,13 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 @SuppressLint("NewApi")
 public class GiaiTri_fm extends Activity{
+	ImageView imggif;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.giaitri_fgm);
+		imggif=(ImageView)findViewById(R.id.imageViewanh);
+		imggif.setBackgroundResource(R.drawable.anhdong);
 	}
 	
 }
